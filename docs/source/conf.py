1715d86d4573c658b2cd1414a81d4a4541ed6cec
Soren Norbaek <sorennorbaek@gmail.com>
2025-08-16 12:50:37 +0200

    feat: refactor pre-commit hooks to use individual checks
    - Replace monolithic quality_gates.py script with individual hooks
    - Add granular hooks for format-check, import-sort-check, lint-check
    - Add separate pre-push hooks for test-coverage and coverage-report
    - Better feedback and faster execution for developers
    - More standard pre-commit approach with individual responsibility
    - Maintain backwards compatibility for CI quality-check script

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../minigun'))
+sys.path.insert(0, os.path.abspath("../minigun"))
 
 # -- Project version ---------------------------------------------------------
 import minigun
@@ -22,21 +22,21 @@ release = str(minigun.__version__)
 
 # -- Project information -----------------------------------------------------
 
-project = 'Minigun'
-copyright = '2022, Soren Norbaek'
-author = 'Soren Norbaek'
+project = "Minigun"
+copyright = "2022, Soren Norbaek"
+author = "Soren Norbaek"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
-autodoc_typehints = 'none'
-autodoc_member_order = 'bysource'
+extensions = ["sphinx.ext.autodoc"]
+autodoc_typehints = "none"
+autodoc_member_order = "bysource"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -49,7 +49,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -58,17 +58,17 @@ html_static_path = []
 
 # -- Forward declaring type aliases ------------------------------------------
 autodoc_type_aliases = {
-  'State' : 'minigun.arbitrary.State',
-  'Trimmer' : 'minigun.trim.Trimmer',
-  'Stream' : 'minigun.stream.Stream',
-  'Domain' : 'minigun.domain.Domain',
-  'Sample' : 'minigun.quantify.Sample',
-  'Sampler' : 'minigun.quantify.Sampler',
-  'Maybe' : 'minigun.maybe.Maybe',
-  'A' : 'A',
-  'B' : 'B',
-  'C' : 'C',
-  'D' : 'D',
-  'P' : 'P',
-  'R' : 'R',
+    "State": "minigun.arbitrary.State",
+    "Trimmer": "minigun.trim.Trimmer",
+    "Stream": "minigun.stream.Stream",
+    "Domain": "minigun.domain.Domain",
+    "Sample": "minigun.quantify.Sample",
+    "Sampler": "minigun.quantify.Sampler",
+    "Maybe": "minigun.maybe.Maybe",
+    "A": "A",
+    "B": "B",
+    "C": "C",
+    "D": "D",
+    "P": "P",
+    "R": "R",
 }
